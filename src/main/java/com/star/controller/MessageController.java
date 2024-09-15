6050c15c2292a9591b815fd3119239beab20eeb2
mcl <mcl@163.com>
2024-09-15 16:32:49 +0800

    test

--- src/main/java/com/star/controller/MessageController.java
+++ src/main/java/com/star/controller/MessageController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -32,6 +33,12 @@ public class MessageController {
 
     @GetMapping("/message")
     public String message() {
+        return "message";
+    }
+
+    @GetMapping("/message-test")
+    public String message(@RequestParam(required = false) String id,
+                          @RequestParam(required = false) String method) {
         return "message";
     }
 
